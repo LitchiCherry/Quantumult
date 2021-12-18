7493bc6995a27bac063030011b7c0bfb456efb7a
沃柑柠檬 <31650171+solologo@users.noreply.github.com>
2021-12-18 12:57:00 +0800

    Update renrenyingshi_tab.js

--- JS/renrenyingshi_tab.js
+++ JS/renrenyingshi_tab.js
@@ -15,17 +15,17 @@ body['data']['homeBarPage'] = [
         "name" : "首页"
       },
       {
-        "selPag" : "",
-        "pageType" : 1,
-        "darkUnselImg" : "",
+        "selPag" : "http://img.rr.tv/other/other/20210914/o_4fd6087388814b48a171e8789ab8f3b1.pag",
+        "pageType" : 0,
+        "darkUnselImg" : "http://img.rr.tv/img/img/20211013/o_dd8bfbd83f0049b5adf4b0e2fcbafe24.png",
         "webUrl" : "",
-        "selImg" : "",
-        "unselImg" : "",
+        "selImg" : "http://img.rr.tv/img/img/20211013/o_2ededc9a9d6c4b818259ac0f72c1fcbb.png",
+        "unselImg" : "http://img.rr.tv/img/img/20211013/o_af287b5caf824367b5dd7435501f684e.png",
         "index" : 2,
-        "nativeAlias" : "amwayVideo",
-        "darkSelImg" : "",
-        "darkSelPag" : "",
-        "name" : ""
+        "nativeAlias" : "community",
+        "darkSelImg" : "http://img.rr.tv/img/img/20211013/o_48f63c0f4c9f459f94bf4f2429054180.png",
+        "darkSelPag" : "http://img.rr.tv/other/other/20210914/o_6bcf3d9a3ae640938884d1574a64f654.pag",
+        "name" : "广场"
       },
       {
         "selPag" : "http://img.rr.tv/barSelpag/20210310/o_1615344143099.pag",
@@ -38,20 +38,7 @@ body['data']['homeBarPage'] = [
         "nativeAlias" : "vip",
         "darkSelImg" : "http://img.rr.tv/img/img/20211013/o_ad54fb0336764d38910304488804f2a1.png",
         "darkSelPag" : "http://img.rr.tv/barSelpag/20210310/o_1615344145409.pag",
-        "name" : "VIP"
-      },
-      {
-        "selPag" : "",
-        "pageType" : 1,
-        "darkUnselImg" : "",
-        "webUrl" : "",
-        "selImg" : "",
-        "unselImg" : "",
-        "index" : 4,
-        "nativeAlias" : "community",
-        "darkSelImg" : "",
-        "darkSelPag" : "",
-        "name" : ""
+        "name" : "会员"
       },
       {
         "selPag" : "http://img.rr.tv/barSelpag/20210310/o_1615344153652.pag",
@@ -60,7 +47,7 @@ body['data']['homeBarPage'] = [
         "webUrl" : "",
         "selImg" : "http://img.rr.tv/img/img/20211013/o_2e5efef7fd654e3ea8a60e3e719e6e8c.png",
         "unselImg" : "http://img.rr.tv/img/img/20211013/o_7492019b1603486383733b7154e967b3.png",
-        "index" : 5,
+        "index" : 4,
         "nativeAlias" : "my",
         "darkSelImg" : "http://img.rr.tv/img/img/20211013/o_f98a74489e3d4dc9bf8651eea65b949e.png",
         "darkSelPag" : "http://img.rr.tv/barSelpag/20210310/o_1615344154522.pag",
