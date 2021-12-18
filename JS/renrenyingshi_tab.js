71fb9fa074af3302e26bcf168e1aea3174373459
沃柑柠檬 <31650171+solologo@users.noreply.github.com>
2021-12-18 12:01:38 +0800

    Update renrenyingshi_tab.js

--- JS/renrenyingshi_tab.js
+++ JS/renrenyingshi_tab.js
@@ -26,19 +26,6 @@ body['data']['homeBarPage'] = [
         "darkSelImg" : "http://img.rr.tv/img/img/20211013/o_ad54fb0336764d38910304488804f2a1.png",
         "darkSelPag" : "http://img.rr.tv/barSelpag/20210310/o_1615344145409.pag",
         "name" : "VIP"
-      },
-      {
-        "selPag" : "http://img.rr.tv/barSelpag/20210310/o_1615344153652.pag",
-        "pageType" : 0,
-        "darkUnselImg" : "http://img.rr.tv/img/img/20211013/o_e35b6941e4a0429486cd5b535337544f.png",
-        "webUrl" : "",
-        "selImg" : "http://img.rr.tv/img/img/20211013/o_2e5efef7fd654e3ea8a60e3e719e6e8c.png",
-        "unselImg" : "http://img.rr.tv/img/img/20211013/o_7492019b1603486383733b7154e967b3.png",
-        "index" : 3,
-        "nativeAlias" : "my",
-        "darkSelImg" : "http://img.rr.tv/img/img/20211013/o_f98a74489e3d4dc9bf8651eea65b949e.png",
-        "darkSelPag" : "http://img.rr.tv/barSelpag/20210310/o_1615344154522.pag",
-        "name" : "我的"
       }
 ]
 body = JSON.stringify(body)
