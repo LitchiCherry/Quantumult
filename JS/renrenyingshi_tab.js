4a8b651a4366c578eff60b19fd7e156f50461946
沃柑柠檬 <31650171+solologo@users.noreply.github.com>
2021-12-18 12:19:20 +0800

    Update renrenyingshi_tab.js

--- JS/renrenyingshi_tab.js
+++ JS/renrenyingshi_tab.js
@@ -13,19 +13,6 @@ body['data']['homeBarPage'] = [
         "darkSelImg" : "http://img.rr.tv/cover/20210310/o_1615343985261.png",
         "darkSelPag" : "http://img.rr.tv/barSelpag/20210310/o_1615343956536.pag",
         "name" : "首页"
-      },
-      {
-        "selPag" : "http://img.rr.tv/barSelpag/20210310/o_1615344143099.pag",
-        "pageType" : 0,
-        "darkUnselImg" : "http://img.rr.tv/img/img/20211013/o_ca5e3b6ce6b747e992789e55222445f2.png",
-        "webUrl" : "",
-        "selImg" : "http://img.rr.tv/img/img/20211013/o_14667f24618841a1906fbb2233b8869a.png",
-        "unselImg" : "http://img.rr.tv/img/img/20211013/o_963933dfc49b4c8bab6e3b36a8a2b42b.png",
-        "index" : 2,
-        "nativeAlias" : "vip",
-        "darkSelImg" : "http://img.rr.tv/img/img/20211013/o_ad54fb0336764d38910304488804f2a1.png",
-        "darkSelPag" : "http://img.rr.tv/barSelpag/20210310/o_1615344145409.pag",
-        "name" : "会员"
       }
 ]
 body = JSON.stringify(body)
